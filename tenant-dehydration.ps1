e6ceae3869c57c27dd9cae276f3e674f8bc25bc5
Dennis <93551312+neutronsec@users.noreply.github.com>
2022-10-11 02:37:45 +0200

    fixed

--- tenant-dehydration.ps1
+++ tenant-dehydration.ps1
@@ -1,44 +1,6 @@
 <#
-A brand new Microsoft 365 tenant was set up for a customer. After the basic configuration, it was on to the security configurations. 
-Among other things with the setup of a Safe Links policy. However, when setting up this policy, an error message appeared at the very end.
-
+When setting up a new tenant and we want to setup Policies, such as Threat Policies for ExchangeOnline, errors appear.
 This error message appears when the M365 tenant is dehydrated. What does that even mean and how to check?
-#>
-
-# We need ExchangeOnline Module
-```
-Install-Module -Name ExchangeOnlineManagement -Verbose -AllowClobber -Force
-```
-
-```
-Connect-ExchangeOnline
-```
-
-# Check dehydrate status
-```
-Get-OrganizationConfig | Format-List Identity,IsDehydrated
-
-
-Identity     : neutronseclabs.onmicrosoft.com
-IsDehydrated : True
-```
-
-# If output is True, hydrate the tenant
-```
-Enable-OrganizationCustomization
-```
-
-# Check the Status
-```
-Get-OrganizationConfig | Format-List Identity,IsDehydrated
-
-
-Identity     : neutronseclabs.onmicrosoft.com
-IsDehydrated : False
-```
-
-<#
-The tenant is no longer dehydrated and we can create policies such as SafeLinks. But what did we do exactly?
 
 Link:
 https://docs.microsoft.com/en-us/powershell/module/exchange/enable-organizationcustomization?view=exchange-ps
@@ -52,3 +14,22 @@ e.g.
 - Creating a new retention policy or modifying a built-in retention policy.
 - Enabling preset security policies in the Microsoft 365 Security Center.
 #>
+
+# We need ExchangeOnline Module
+
+Install-Module -Name ExchangeOnlineManagement -Verbose -AllowClobber -Force
+
+Connect-ExchangeOnline
+
+
+# Check dehydrate status
+Get-OrganizationConfig | Format-List Identity,IsDehydrated
+
+# If above output is True, hydrate the tenant
+Enable-OrganizationCustomization
+
+# Check the new Status (isdehydrated should be False)
+Get-OrganizationConfig | Format-List Identity,IsDehydrated
+
+
+# The tenant is no longer dehydrated and we can create policies such as SafeLinks. For a in-depth explanation check the link at the top.
